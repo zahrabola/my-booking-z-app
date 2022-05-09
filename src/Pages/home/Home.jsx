1fa491255e4e8d61d53bf7eac3f14f454999f2ff
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-05-09 12:11:58 +0100

    update home

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Navbar from "../../components/navbar/Navbar";
+import "./Home.css";
+
 const Home = () =>{
     return (
-        <div>Home </div>
+        <div>Home 
+
+            <Navbar />
+        </div>
 
     )
 }
