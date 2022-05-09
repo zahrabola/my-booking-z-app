d4f4c74d7ad0980b5f61e5b0fc3b2baff569aa54
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-05-09 12:30:36 +0100

    update navbar css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,8 +3,16 @@ import "./Navbar.css";
 
 const Navbar= () =>{
     return (
-        <div>Navbar Zahra</div>
-    )
+      <div className="navbar">
+        <div className="navcontainer">
+          <span className="logo">Zahra's Booking </span>
+          <div className="navItems">
+            <button className="navbtn">Register</button>
+            <button className="navbtn">Login</button>
+          </div>
+        </div>
+      </div>
+    );
 }
 
 export default Navbar
