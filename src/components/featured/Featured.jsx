6959b4ca7c7781e2083f4d0f0620f0a5f5e74eb5
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-05-10 11:35:17 +0100

    update feature css

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,8 +1,13 @@
+import "./Featured.css";
+
 const Featured = () => {
   return (
-    <div className="container featured">
+    <div className=" featured">
       <div className="featurecard">
-        <img src="" className="feautre-img" />
+        <img
+          src="https://images.pexels.com/photos/6588993/pexels-photo-6588993.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          className="feautre-img"
+        />
         <div className="featureTitle">
           <h1>London</h1>
           <h2>120 properties</h2>
@@ -10,7 +15,10 @@ const Featured = () => {
       </div>
 
       <div className="featurecard">
-        <img src="" className="feautre-img" />
+        <img
+          src="https://images.pexels.com/photos/3035122/pexels-photo-3035122.jpeg?cs=srgb&dl=pexels-anna-urlapova-3035122.jpg&fm=jpg"
+          className="feautre-img"
+        />
         <div className="featureTitle">
           <h1>Edinburgh </h1>
           <h2>90 properties</h2>
@@ -18,7 +26,10 @@ const Featured = () => {
       </div>
 
       <div className="featurecard">
-        <img src="" className="feautre-img" />
+        <img
+          src="https://images.pexels.com/photos/6029313/pexels-photo-6029313.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          className="feautre-img"
+        />
         <div className="featureTitle">
           <h1>Cardiff</h1>
           <h2>160 properties</h2>
