d64414dbbb3aaf467ea13206abe3fd928060fe50
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-05-13 14:27:48 +0100

    Update Hotel

--- src/Pages/hotel/Hotel.jsx
+++ src/Pages/hotel/Hotel.jsx
@@ -1,5 +1,31 @@
 import React from "react";
+import "./Hotel.css";
+import Navbar from "../../components/navbar/Navbar";
+import Header from "../../components/header/Header";
+import { FaSearchLocation} from "react-icons/fa";
 const Hotel = () => {
-  return <div>Hotel</div>;
+  return (
+    <div>
+      <Navbar />
+      <Header type="list" />
+      <div className="hotelcontainer">
+        <div className="hotelwrapper">
+          <h1 className="hoteltitle">London Hotel</h1>
+          <div className="hoteladress">
+            <div className="hotelicon">
+              <FaSearchLocation size="1.3em" />
+              <span>Elton street, London </span>
+            </div>
+          </div>
+          <span className="hoteldistance">
+            Excellent location – 500m from center
+          </span>
+          <span className="hotelPH">
+            Book a stay over £114 at this property and get a free airport taxi
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Hotel;
